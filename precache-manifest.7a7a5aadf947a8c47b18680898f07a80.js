bf6a204636e7e6bc7e67f476103291e03e614ac5
Inbarasankumar <inbarasanraja@gmail.com>
2019-09-17 15:56:05 +0530

    Updates

---
+++ precache-manifest.7a7a5aadf947a8c47b18680898f07a80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee9fc9d0e652769e6d1bb34683f28f9f",
+    "url": "/Matching_Game/index.html"
+  },
+  {
+    "revision": "fd6b3ad01609ede6af14",
+    "url": "/Matching_Game/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "a4d19328997eb1029ea7",
+    "url": "/Matching_Game/static/js/2.526878be.chunk.js"
+  },
+  {
+    "revision": "fd6b3ad01609ede6af14",
+    "url": "/Matching_Game/static/js/main.c98d7b51.chunk.js"
+  },
+  {
+    "revision": "1f933ce90bfa72f5fb31",
+    "url": "/Matching_Game/static/js/runtime~main.13bd4878.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Matching_Game/static/media/logo.5d5d9eef.svg"
+  }
+]);
